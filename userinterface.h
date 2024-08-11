a1986b07f6798598e408e22922baed882bfc2303
Kobe Dieryck <kobe.dieryck@icloud.com>
2024-08-11 15:18:55 +0200

    Working bar, no function yet

--- userinterface.h
+++ userinterface.h
@@ -1,17 +1,36 @@
 #ifndef USERINTERFACE_H
 #define USERINTERFACE_H
-#include <QWidget>
+
+#include <QMainWindow>
 
 class TcpClient;
 
-class Userinterface
+class Userinterface : public QMainWindow
 {
+    Q_OBJECT
+
 public:
-    Userinterface(TcpClient * client);
+    Userinterface(TcpClient *client);
     ~Userinterface(){};
+
+private slots:
+    void saveFile();
+    void loadFile();
+    void exitApp();
+
 private:
-    TcpClient * Client;
-    QWidget window;
+    void createMenus();
+
+    TcpClient *Client;
+
+    // New Menu Elements
+    QMenu *fileMenu;
+    QAction *saveAction;
+    QAction *loadAction;
+    QAction *exitAction;
+
+    // Other UI elements go here, like text edit, etc.
 };
 
 #endif // USERINTERFACE_H
+
